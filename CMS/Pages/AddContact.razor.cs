bc50f2cb13a19c39e09d42db593ce88da2e8a9b5
pschuber <pschuber@umich.edu>
2022-05-31 23:06:52 -0400

    added stuff

---
+++ CMS/Pages/AddContact.razor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using CMS.Data;
+using CMS.DB;
+using CMS.FormModels;
+using Microsoft.AspNetCore.Components;
+
+namespace CMS.Pages
+{
+	public class AddContactBase: ComponentBase
+	{
+        protected ContactModel newContactForm = new ContactModel();
+
+
+        [Inject]
+        NavigationManager NavigationManager { get; set; }
+
+        [Inject]
+        ContactService ContactService { get; set; }
+
+        protected void cancelHandler()
+        {
+            NavigationManager.NavigateTo("");
+        }
+        protected async void submitHandler()
+        {
+            Contact newContact = new Contact();
+            newContact.LastName = newContactForm.LastName;
+            newContact.FirstName = newContactForm.FirstName;
+            newContact.PhoneNumber = newContactForm.PhoneNumber;
+            newContact.BirthDate = newContactForm.BirthDate;
+
+
+            await ContactService.AddContact(newContact);
+
+            NavigationManager.NavigateTo("");
+        }
+    }
+}
+
